a486950d60c9ea0ec803e51f175db4704756519f
Brandon Troche <bttroche@gmail.com>
2015-09-28 18:49:32 -0400

    In the process of figuring out the best architecture for the now left and right nodes for movement. The key is for the buttons to be invisible so clicking on the left of the screen moves left and clicking on the right of the screen moves right. To do this I want it to ideally change the gravity. I have tried multiple different types of inheritance and calling and experimented with creating extensions and protocols, which seems like the way to go and shall be worked on for the next implementation.
    Even if it does not look like much has changed, a lot of work and debugging has gone into this from the past repository to this one on playing with the physics node and fixing engine breaks, etc...

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -5,36 +5,39 @@ import Foundation
     Positive x value gravity pushes ---> this way
 */
 
-class MainScene: CCNode {
+class MainScene: CCNode, CCPhysicsCollisionDelegate {
 
-    weak var clickYo:CCNodeGradient!
+//    weak var gamePhysicsNode:CCPhysicsNode!
     weak var gamePhysicsNode:CCPhysicsNode!
+    weak var left:LeftButton!
+    weak var right:RightButton!
+    
+    var gravity: Int = 0
+    
     
     func didLoadFromCCB() {
         userInteractionEnabled = true       //Start user interaction
-        gamePhysicsNode.debugDraw = true        //Show outline of all physics bodies
+        gamePhysicsNode.debugDraw = true    //Show outline of all physics bodies
     }
     
-    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!){
-        var color:CCColor! = CCColor(red:20.0/255, green:120.0/255, blue:120.0/255)
+//    override func update(delta: CCTime) {
+//        gravity = left.
+//        gamePhysicsNode.gravity.x = gravity
+//    }
+    
+//    func Left(){
+//        self.gamePhysicsNode.gravity.x += -600    //placeholder test for negative gravity influence
+//        println(gamePhysicsNode.gravity.x)      //print to console to check the value is changing
+//    }
+    
+//    func Right(){
+//        self.gamePhysicsNode.gravity.x += 600        //placeholder test for positive gravity influence
+//        println(gamePhysicsNode.gravity.x)      //print to console to check the value is changing
+//    }
+    
+//    func Reset(){
+//        gamePhysicsNode.gravity.x = 0      //reset horizontal gravity to 0 for play testing purposes
+//    }
 
-        clickYo.color = color
-        
-    }
-    
-    func Left(){
-        gamePhysicsNode.gravity.x += -20    //placeholder test for negative gravity influence
-        println(gamePhysicsNode.gravity.x)      //print to console to check the value is changing
-    }
-    
-    func Right(){
-        gamePhysicsNode.gravity.x += 20        //placeholder test for positive gravity influence
-        println(gamePhysicsNode.gravity.x)      //print to console to check the value is changing
-
-    }
-    
-    func Reset(){
-        gamePhysicsNode.gravity.x = 0       //reset horizontal gravity to 0 for play testing purposes 
-    }
-    
 }
+
